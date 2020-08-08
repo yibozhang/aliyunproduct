9826ff97e8153647bc18a4f6fa85f050a49065b0
zhang_15127648471@163.com <zhang_15127648471@163.com>
2020-08-08 09:52:06 +0800

    直播风险评估

---
+++ RiskAssesment/liveStream.py
@@ -0,0 +1,109 @@
+# -*- coding:utf-8 -*-
+import threading
+import asyncio
+import time
+import urllib3 
+import json
+from aliyun.log import *
+from aliyun.log.util import base64_encodestring
+from random import randint
+
+flag=0
+stime=int(time.time()) - 43200
+etime=int(time.time())
+cdnsky="cdnskyeye.alibaba-inc.com"
+http = urllib3.PoolManager()
+allStrategyID={}
+strategyResult=[]
+
+
+# corotine 协程 
+@asyncio.coroutine
+def hello(host):
+
+ global flag
+ if flag == 100:
+  time.sleep(1)
+  flag=0
+ flag += 1
+ yield printd(host) 
+ queryResult(allStrategyID)
+
+
+# 捞起实例子ID 
+
+def printd(host):
+ url="http://cdnskyeye.alibaba-inc.com/api/lualib/diagnose.diagnose/start_diagnose?strategy_id=247&author=%E9%9F%A9%E7%AC%A0&LiveDomain=" + host + f"&StartTime={stime}&EndTime={etime}"
+ resp = http.request('GET',url)
+ try:
+  if resp.data: 
+   msg = json.loads(resp.data.decode())['content']['instance_id']
+  allStrategyID[host] = msg
+ except Exception as e:
+  print(e)
+
+
+# 循环分析诊断结果
+
+def queryResult(id):
+ print("[DebugLength]->",len(id))
+ 
+ if not id:
+  exit()
+ while True:
+  for key in list(id.keys()):
+   url = f"http://cdnskyeye.alibaba-inc.com/api/lualib/diagnose.diagnose/diagnose_instance?instance_id={id[key]}"
+   resp = http.request('GET',url)
+   if resp.data:
+    msg = json.loads(resp.data.decode())
+    if msg:
+     strategyResult.append((msg['content'][0]['result'][0]))
+     id.pop(key)
+  if not id:
+   logTail(strategyResult)
+   break 
+
+# 写入 SLS  log
+
+def logTail(result):
+ print("[Debug LogTail]->",len(result))
+ endpoint = "cn-zhangjiakou.log.aliyuncs.com"
+ accessKeyId = "LTAI4GDkNfUA8rBGRmzCUS9w"
+ accessKey = "OPOOM7q6Yfe9wDrpN4TAhhjjgVAIFa"
+ logstore = "livestream"
+ project = "v-cloud-data"
+ client = LogClient(endpoint, accessKeyId, accessKey)
+ topic = '直播业务'
+ source = ''
+ logitemList = []
+ for i in range(len(result)):
+  contents = [
+        ('product_name', 'livestream'),
+        ('ali_uid',json.loads(result[i]['result'])['uid']),
+        ('start_time', str(stime)),
+        ('end_time', str(etime)),
+        ('domain',json.loads(result[i]['result'])['domain']),
+        ('status',json.loads(result[i]['result'])['status']),
+        ('solution',json.loads(result[i]['result'])['solution']),
+        ('officialResponse',json.loads(result[i]['result'])['officialResponse']),
+        ('desc',json.loads(result[i]['result'])['desc']),
+        ('detail',json.loads(result[i]['result'])['detail'])
+  ]
+  logItem = LogItem()
+  logItem.set_time(int(time.time()))
+  logItem.set_contents(contents)
+  logitemList.append(logItem)
+  if len(logitemList) >= 1000:
+   request = PutLogsRequest(project, logstore, topic, source, logitemList)
+   response = client.put_logs(request)
+   response.log_print()
+   logitemList = []
+ request = PutLogsRequest(project, logstore, topic, source, logitemList)
+ response = client.put_logs(request)
+ response.log_print()
+
+files = open('live_domain.lst','r')
+loop = asyncio.get_event_loop()
+tasks = [hello(i.strip('\n')) for i in files.readlines()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
