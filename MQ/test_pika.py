724ed30bb00810f10bfc1fcd7df565a3c1b9d657
yibozhang <zhang_15127648471@163.com>
2025-01-30 11:51:58 +0800

    Create test_pika.py
    create test_pika

---
+++ MQ/test_pika.py
@@ -0,0 +1,21 @@
+#-*-coding:utf8-*-
+import pika
+
+config = pika.ConnectionParameters(
+    host='amqp-cn-2r42a2eku009.mq-amqp.cn-hangzhou-249959-a.aliyuncs.com',
+    credentials=pika.PlainCredentials('amqp-cn-2r42a2eku009', 'Qjc2QjFEMjU4OTc4Q0Q1RUQ2MTQ3NEU2RkEzQjU5ODc0ODhDMjkzQjoxNjI3MzQ5NTUwNDk4'),
+)
+
+conn = pika.BlockingConnection(config)
+channel = conn.channel()
+
+# 修改 type 为 topic
+channel.exchange_declare(exchange='amq.topic', type='topic')
+
+channel.basic_publish(
+    exchange='amq.topic',
+    routing_key='hanli_key',
+    body='Hello World!'
+)
+
+conn.close()
